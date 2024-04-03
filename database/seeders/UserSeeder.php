67183d8978e1079f779e97109826093805f1ee40
mukhlish32@ <mukhlish.syarif@gmail.com>
2024-04-03 21:12:18 +0700

    custom: add seed for users

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -15,13 +16,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'id' => 1,
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('secret'),
-            'created_at' => now(),
-            'updated_at' => now()
-        ]);
+        $users = [
+            ['name' => 'Muhammad Mukhlish Syarif', 'email' => 'mukhlish@email.com', 'phone' => '1234567890', 'address' => 'Jl Swadaya No. 093'],
+            ['name' => 'Rosa Madina', 'email' => 'rosa@email.com', 'phone' => 'abc123', 'address' => 'Kota Melati'],
+            ['name' => 'Arina Widiawati', 'email' => 'wiwid@email.com', 'phone' => null, 'address' => null],
+            ['name' => 'Muhammad Agung Armansyah', 'email' => 'agung@ymail.com', 'phone' => '123456', 'address' => 'Kota Mawar'],
+        ];
+
+        foreach ($users as $userData) {
+            $userData['created_at'] = Carbon::now();
+            DB::table('users')->insert($userData);
+        }
     }
 }
